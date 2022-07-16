ac9158ecb97a57e5ae7bb1c59d356d0807d5d03d
MirrorCube <joycemai.exe@gmail.com>
2022-07-16 00:15:18 -0700

    Move and Dash Demo

--- Assets/Scripts/Joyce/MovementScript.cs
+++ Assets/Scripts/Joyce/MovementScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MovementScript : MonoBehaviour
 {
+    // movement variables
     [SerializeField] private float speed = 5f;
     [SerializeField] private float dashDis = 2;
 
@@ -12,39 +13,110 @@ public class MovementScript : MonoBehaviour
 
     [SerializeField] private Animator anim;
 
-    private Vector3 left = new Vector3(0, 0, 90);
-    private Vector3 right = new Vector3(0, 0, -90);
-    private Vector3 up = Vector3.zero;
-    private Vector3 down = new Vector3(0, 0, 180);
+    //dashing variables
+    [SerializeField] private float dashForce = 5f;
+    [SerializeField] private float dashTime = 2f;
+    [SerializeField] private float dashCooldown = 5f;
+    private bool dashing = false;
+    private bool canDash = true;
+    [SerializeField] private TrailRenderer trail;
 
-  
+    private enum Dir { LEFT, RIGHT, UP, DOWN}
+    private Dir playerDir = Dir.UP;
+
+    private void Start()
+    {
+        dashing = false;
+        canDash = true;
+        playerDir = Dir.UP;
+    }
+
     void Update()
     {
-       // priority on x movement
+        if (dashing)
+        {
+            return;
+        }
+
+        // resets to up if not given a direction input
+        playerDir = Dir.UP;
+
+       // priority on x movement // also keeps track of direction to dash
         movement.x = Input.GetAxisRaw("Horizontal");
         if (movement.x == 0)
         {
             movement.y = Input.GetAxisRaw("Vertical");
+            if(movement.y > 0)
+            {
+                playerDir = Dir.UP;
+            }
+            else if (movement.y < 0)
+            {
+                playerDir = Dir.DOWN;
+            }
         }
         else
         {
             movement.y = 0;
+            if (movement.x > 0)
+            {
+                playerDir = Dir.RIGHT;
+            }
+            else if (movement.x < 0)
+            {
+                playerDir = Dir.LEFT;
+            }
         }
+
 
         // controls the animator
         anim.SetFloat("Horizontal", movement.x);
         anim.SetFloat("Vertical", movement.y);
         anim.SetFloat("Speed", movement.sqrMagnitude);
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetKeyDown(KeyCode.Space) && canDash)
         {
-            print("dash?");
+            StartCoroutine(Dash());
         }
         
     }
 
     private void FixedUpdate()
     {
+        if (dashing)
+        {
+            return;
+        }
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+
+    private IEnumerator Dash()
+    {
+        canDash = false;
+        dashing = true;
+        switch (playerDir)
+        {
+            case Dir.LEFT:
+                rb.velocity = new Vector2(-dashForce, 0f);
+                break;
+            case Dir.RIGHT:
+                rb.velocity = new Vector2(dashForce, 0f);
+                break;
+            case Dir.DOWN:
+                rb.velocity = new Vector2(0f, -dashForce);
+                break;
+            default:
+                rb.velocity = new Vector2(0f, dashForce);
+                break;
+        }
+
+        trail.emitting = true;
+        yield return new WaitForSeconds(dashTime);
+        dashing = false;
+
+        trail.emitting = false;
+        yield return new WaitForSeconds(dashCooldown);
+        canDash = true;
+
     }
 }
