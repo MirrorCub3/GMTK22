2fc69bb0eab8e369f07d0fd1fea86e83cfa48510
Zaphero <menontos@gmail.com>
2022-07-16 20:38:00 -0500

    Trying to figure out why bullets flash

---
+++ Assets/BulletSpeed.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletSpeed : MonoBehaviour
+{
+    private Vector2 SetVelocity;
+    /*void FixedUpdate()
+    {
+        transform.position = transform.forward * Time.deltaTime;
+    }*/
+}
